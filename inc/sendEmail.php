05f13c9793049cb1b6701ea53bd52c6a3352c365
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-04-23 22:14:15 +0530

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,35 +1,35 @@
 <?php
 
-require 'vendor/autoload.php'; // Make sure to update the path to the PHPMailer library
-
 // Replace this with your own email address
-$siteOwnersEmail = 'juhinsohamdas@ieee.org';
+$siteOwnersEmail = 'juhinsohamdas@gmail.com';
+
 
 if($_POST) {
+
    $name = trim(stripslashes($_POST['contactName']));
    $email = trim(stripslashes($_POST['contactEmail']));
    $subject = trim(stripslashes($_POST['contactSubject']));
    $contact_message = trim(stripslashes($_POST['contactMessage']));
 
    // Check Name
-   if (strlen($name) < 2) {
-      $error['name'] = "Please enter your name.";
-   }
-   // Check Email
-   if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $error['email'] = "Please enter a valid email address.";
-   }
-   // Check Message
-   if (strlen($contact_message) < 15) {
-      $error['message'] = "Please enter your message. It should have at least 15 characters.";
-   }
+	if (strlen($name) < 2) {
+		$error['name'] = "Please enter your name.";
+	}
+	// Check Email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Please enter a valid email address.";
+	}
+	// Check Message
+	if (strlen($contact_message) < 15) {
+		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+	}
    // Subject
-   if ($subject == '') { $subject = "Contact Form Submission"; }
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
 
    // Set Message
-   $message = "";
    $message .= "Email from: " . $name . "<br />";
-   $message .= "Email address: " . $email . "<br />";
+	$message .= "Email address: " . $email . "<br />";
    $message .= "Message: <br />";
    $message .= $contact_message;
    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
@@ -37,32 +37,33 @@ if($_POST) {
    // Set From: header
    $from =  $name . " <" . $email . ">";
 
-   // Create a new PHPMailer instance
-   $mail = new PHPMailer\PHPMailer\PHPMailer();
-
-   // SMTP settings (replace with your own)
-   $mail->isSMTP();
-   $mail->Host = 'smtp.gmail.com';
-   $mail->Port = 587;
-   $mail->SMTPSecure = 'tls';
-   $mail->SMTPAuth = true;
-   $mail->Username = 'your_email_address';
-   $mail->Password = 'your_email_password';
-
    // Email Headers
-   $mail->setFrom($email, $name);
-   $mail->addAddress($siteOwnersEmail, 'Site Owner');
-   $mail->isHTML(true);
-   $mail->Subject = $subject;
-   $mail->Body = $message;
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
 
    if (!$error) {
-      if($mail->send()) {
-         echo "OK";
-      }
-      else {
-         echo "Something went wrong. Please try again.";
-      }
-   }
-   else {
-      $response = (isset($error['name'])) ? $error['name'] . "<br />"
+
+      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
+		
+	} # end if - no validation error
+
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+		
+		echo $response;
+
+	} # end if - there was a validation error
+
+}
+
+?>
