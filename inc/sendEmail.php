e102b622b84d6b938506631519f52aef89f162d1
SOHAM DAS <54598380+sohamjuhin@users.noreply.github.com>
2023-04-23 22:49:07 +0530

    Update sendEmail.php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -3,53 +3,53 @@
 // Replace this with your own email address
 $siteOwnersEmail = 'juhinsohamdas@gmail.com';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_POST) {
 
-    $name = filter_var(trim($_POST['contactName']), FILTER_SANITIZE_STRING);
-    $email = filter_var(trim($_POST['contactEmail']), FILTER_SANITIZE_EMAIL);
-    $subject = filter_var(trim($_POST['contactSubject']), FILTER_SANITIZE_STRING);
-    $contact_message = filter_var(trim($_POST['contactMessage']), FILTER_SANITIZE_STRING);
+    $name = trim($_POST['contactName']);
+    $email = trim($_POST['contactEmail']);
+    $subject = trim($_POST['contactSubject']);
+    $message = trim($_POST['contactMessage']);
 
-    $errors = [];
-
-    // Validate Name
-    if (empty($name)) {
-        $errors[] = "Please enter your name.";
+    // Check Name
+    if (strlen($name) < 2) {
+        $error['name'] = "Please enter your name.";
     }
 
-    // Validate Email
-    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = "Please enter a valid email address.";
+    // Check Email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error['email'] = "Please enter a valid email address.";
     }
 
-    // Validate Message
-    if (empty($contact_message) || strlen($contact_message) < 15) {
-        $errors[] = "Please enter your message. It should have at least 15 characters.";
+    // Check Message
+    if (strlen($message) < 15) {
+        $error['message'] = "Please enter your message. It should have at least 15 characters.";
     }
 
-    // Set Default Subject
+    // Set Subject
     if (empty($subject)) {
         $subject = "Contact Form Submission";
     }
 
-    // Set Email Message
-    $message = "Email from: $name<br/>";
-    $message .= "Email address: $email<br/>";
-    $message .= "Message:<br/> $contact_message <br/>";
-    $message .= "<br/>-----<br/>This email was sent from your site's contact form.<br/>";
+    if (empty($error)) {
 
-    // Set Email Headers
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        // Set From: header
+        $from = $name . " <" . $email . ">";
 
-    if (empty($errors)) {
+        // Email Headers
+        $headers = "From: " . $from . "\r\n";
+        $headers .= "Reply-To: " . $email . "\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+        // Set Message
+        $body = "Email from: " . $name . "<br />";
+        $body .= "Email address: " . $email . "<br />";
+        $body .= "Message: <br />";
+        $body .= $message;
+        $body .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
 
         // Send Email
-        $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-
-        if ($mail) {
+        if (mail($siteOwnersEmail, $subject, $body, $headers)) {
             echo "OK";
         } else {
             echo "Something went wrong. Please try again.";
@@ -57,8 +57,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     } else {
         // Validation Errors
-        foreach ($errors as $error) {
-            echo $error . "<br/>";
-        }
+        $response = "";
+        $response .= isset($error['name']) ? $error['name'] . "<br />\n" : "";
+        $response .= isset($error['email']) ? $error['email'] . "<br />\n" : "";
+        $response .= isset($error['message']) ? $error['message'] . "<br />\n" : "";
+        echo $response;
     }
+
 }
